0a6f0be84e6c580f6f6e54e5e4bd94a104e1f953
Tanvir <tanvirlaravel@gmail.com>
2024-10-11 11:54:16 +0600

    initial commit

---
+++ custom-product-message.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Custom Product Message
+Description: This si my First woocomerce plugin
+Version: 1.0
+Author: Md Tanvirul Islam
+*/
+
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) {
+    exit;
+}
+
